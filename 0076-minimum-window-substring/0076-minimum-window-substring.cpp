cd7d0422cc230a10e17bb8dfcbbdd15c91544034
developertutuorials <ayushkum2005a@gmail.com>
2026-08-15 22:49:16 +0530

    Time: 175 ms (5.89%), Space: 11.8 MB (15.86%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,44 +1,43 @@
 class Solution {
 public:
-    bool right(vector<int> &have,vector<int>&need){
+    bool right(vector<int> &have,vector<int> &need){
+       
         for(int i =0;i<256;i++){
             if(have[i]<need[i]){
                 return false;
             }
-            
         }
         return true;
     }
+
     string minWindow(string s, string t) {
-        int n =s.size();
-        int m=t.size();
-        int low=0;
-        int high=0;
-        int res = INT_MAX;
-        int start=-1;
+        int n = s.size();
+        int m =t.size();
         vector<int>have(256,0);
         vector<int>needed(256,0);
+        int low=0;
+        int high=0;
+        int res=INT_MAX;
+        int start= -1;
         for(int i =0;i<m;i++){
             needed[t[i]]++;
         }
         for(high=0;high<n;high++){
             have[s[high]]++;
-            
             while(right(have,needed)){
-                int len = high-low+1;
-                if(res>len){
+                int len=high-low+1;
+                if(len<res){
                     res=len;
                     start=low;
                 }
                 have[s[low]]--;
-                
                 low++;
-                
-                
+               
+
             }
-            
         }
         return (res==INT_MAX)?"":s.substr(start,res);
+
         
     }
 };
