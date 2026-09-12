17f08b163500c89924671002447b3cde9336569f
developertutuorials <ayushkum2005a@gmail.com>
2026-09-13 05:00:33 +0530

    Time: 0 ms (100%), Space: 8.2 MB (17.22%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
+
+    unordered_map<int,int>dp;
     int fib(int n) {
         if(n==0 || n==1){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        if(dp.find(n)!=dp.end()){
+            return dp[n];
+        }
+        int a1 = fib(n-1);
+        int a2 = fib(n-2);
+        int ans = a1+a2;
+        dp[n]=ans;
+        return ans;
     }
 };
