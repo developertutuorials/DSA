43c7bf629ddf9b85d4beef75d72ae8d2adff493a
developertutuorials <ayushkum2005a@gmail.com>
2026-09-12 09:05:31 +0530

    Time: 0 ms (100%), Space: 9.1 MB (77.31%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    void fun(string &digits,int n ,int idx,string &dairy,vector<string> &res,unordered_map<char,string> &f){
+        if(idx==n){
+            res.push_back(dairy);
+            return;
+        }
+
+        string choice =f[digits[idx]];
+        for(int j =0;j<choice.size();j++){
+            dairy.push_back(choice[j]);
+            fun(digits,n,idx+1,dairy,res,f);
+            dairy.pop_back();
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        unordered_map<char,string>f;
+        f['2'] = "abc";
+        f['3'] = "def";
+        f['4'] = "ghi";
+        f['5'] = "jkl";
+        f['6'] = "mno";
+        f['7'] = "pqrs";
+        f['8'] = "tuv";
+        f['9'] = "wxyz";
+  
+      int n =digits.size();
+      int idx =0;
+      string dairy;
+      vector<string> res;
+      fun(digits,n,idx,dairy,res,f);
+      return res;  
+    }
+};
